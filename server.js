6d67d72c3f9ab62c148bd977b0fbd27de11948ae
JamesDoane <ijamesdoaneok@gmail.com>
2021-08-31 11:24:52 -0600

    initialize procfile

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-const port = process.env
+const port = process.env.PORT || 4545
 
 app.listen(port, () => console.log(`4545 is the port you're looking for`))
