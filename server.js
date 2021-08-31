d803cf748a105b8b8b38e91a0e3f5d74cad5c79c
JamesDoane <ijamesdoaneok@gmail.com>
2021-08-31 11:20:32 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+const port = process.env
+
+app.listen(port, () => console.log(`4545 is the port you're looking for`))
